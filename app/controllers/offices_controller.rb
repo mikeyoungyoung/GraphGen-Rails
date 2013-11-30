cb9abff3c5ec0e8aaa6a20ea92ec5f89bb1122e6
mikeyoungyoung <mikeyoung.young@gmail.com>
2013-11-30 10:59:32 -0500

    office linkage

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,2 +1,7 @@
 class OfficesController < ApplicationController
+    def create
+        @partner = Partner.find(params[:partner_id])
+        @office = @partner.offices.create(params[:office].permit(:street, :number, :city, :postal_code))
+        redirect_to partner_path(@partner)
+    end
 end
